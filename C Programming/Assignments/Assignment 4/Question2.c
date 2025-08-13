0e8c72be3ae1141c8313837106e2a373aa61a78d
Kshitij <160639154+KshitijKhande@users.noreply.github.com>
2025-08-13 18:05:37 +0530

    Add files via upload

---
+++ C Programming/Assignments/Assignment 4/Question2.c
@@ -0,0 +1,21 @@
+//2. Print prime numbers in the given range 1 to n.//
+
+#include <stdio.h>
+void main() {
+	int no;
+    printf("Enter a number:");
+    scanf("%d",&no);
+    printf("Prime Numbers between 1 to %d are: \n",no);
+    
+	for(int i=2;i<no;i++){
+        int flag = 1;
+        for (int j = 2; j * j <= i; j++) {
+            if (i % j == 0) {
+                flag = 0;
+            }
+        }
+
+    if (flag)
+        printf(" %d ", i);
+	}
+}
