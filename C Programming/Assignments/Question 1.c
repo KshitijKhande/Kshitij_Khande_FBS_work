bb43a8de62e71864ab5314c8abb9d39b44384d0b
Kshitij <160639154+KshitijKhande@users.noreply.github.com>
2025-08-15 22:01:29 +0530

    Add files via upload

---
+++ C Programming/Assignments/Question 1.c
@@ -0,0 +1,19 @@
+//1. Write a program to check whether a number is even or odd.//
+
+#include<stdio.h>
+void evenodd();
+void main(){
+	evenodd();
+} //main end here//
+
+void evenodd(){
+
+	int no=7;
+	if(no%2==0){
+		printf("%d is a even number",no);
+		}else
+		{
+			printf("%d is a odd number",no);
+		
+	}
+}       //function ends here//
